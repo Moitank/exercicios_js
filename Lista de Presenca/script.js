12b457c0fafed814dd8ecc8a785bdfa38ec67b87
moitank <marcelo.moita@al.resilia.com.br>
2020-10-15 01:10:27 -0300

    Novos exercícios adicionados

---
+++ Lista de Presenca/script.js
@@ -0,0 +1,14 @@
+
+
+
+var matriculados = ['Scarlet', 'Moita', 'Erica', 'Thiago', 'Gabriel', 'David']
+
+var lista_presenca = ['Scarlet', 'Moita', 'Thiago']
+
+function calcula_presenca (alunos, presenca) {
+
+    for (i=0 , i<presenca.lenght, i++) {
+
+        alunos.indexOf (presenca[i])
+    }
+}
