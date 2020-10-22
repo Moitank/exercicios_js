e404364bc86d1f69276c2c3d0f62e877fc83a74f
moitank <marcelo.moita@al.resilia.com.br>
2020-10-22 17:49:43 -0300

    Novos exercícios

---
+++ Evento JS/script.js
@@ -0,0 +1,14 @@
+
+
+var btn = document.createElement("button");
+
+btn.innerHTML = "CLICK ME";
+document.body.appendChild(btn);
+
+
+
+
+
+
+
+
--- Semáforo/script.js
+++ Semáforo/script.js
@@ -1,5 +1,22 @@
 
 
 function sinalizar (luz) {
-    if 
+
+    if (luz == 'verde'){
+        return 'Pode passar!'
+    }
+
+    if (luz == 'amarelo'){
+        return 'Melhor freiar!'
+    }
+
+    if(luz == 'vermelho'){
+        return 'Pare já"'
+    }
+
+
 }
+
+document.write(sinalizar('verde') + "<br>");
+document.write(sinalizar('amarelo') + "<br>");
+document.write(sinalizar('vermelho') + "<br>");
